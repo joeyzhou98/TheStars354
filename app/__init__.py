4d5539cfbe8b9f79534516a0a4d79a1029f09105
louisazhao <32183353+louisazhao@users.noreply.github.com>
2019-10-15 13:43:07 -0400

    1. reorganise the code, put routes in resources.py (both secure and non-secure, since in resources.py already has SecureResource class that inherits from Resources. So that security.py only deals with logics) 2. add mariadb and sqlalchemy in pipfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 import os
-from flask import Flask, current_app, send_file, render_template, redirect, request
+from flask import Flask, current_app, send_file
 from flask_sqlalchemy import SQLAlchemy
 
-from .api import api_bp
 from .client import client_bp
 
 app = Flask(__name__, static_folder='../dist/static')
-app.register_blueprint(api_bp)
-# app.register_blueprint(client_bp)
+#need to set up database first before import api_bp
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://comp354:Ll88050517@comp354.cha9oynmedpn.us-east-2.rds.amazonaws.com:3306/comp354'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+from .api import api_bp
+
+app.register_blueprint(api_bp)
+# app.register_blueprint(client_bp)
 
 
 @app.before_first_request
@@ -32,55 +35,3 @@ def index_client():
     return send_file(entry)
 
 
-from .models import *
-
-
-@app.route('/registration', methods=['GET', 'POST'])
-def user_registration():
-    if request.method == 'POST':
-        uname = request.form['uname']
-        mail = request.form['mail']
-        passw = request.form['passw']
-
-        if UserAuthModel.find_by_username(uname):
-            return {'message': 'User name exists.'}
-
-        if UserAuthModel.find_by_useremail(mail):
-            return {'message': 'User email exists.'}
-
-        new_user = UserAuthModel(username=uname, useremail=mail, password=passw)
-        new_user.save_to_db()
-        return {'message': 'saved to database'}
-    return {'message': 'Hit the user registration endpoint, show the registration page.'}
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def user_login():
-    if request.method == "POST":
-        uemail = request.form['uname']
-        passw = request.form['passw']
-
-        login = UserAuthModel.query.filter_by(uemail=UserAuthModel.username, passw=UserAuthModel.password).first()
-        if login is not None:
-            return redirect('/')
-    return {'message': 'Hit the user login endpoint with GET, show the login page'}
-
-
-@app.route('/logout/access', methods=['POST'])
-def logout_access():
-    return {'message': 'Hit the user logout access endpoint.'}
-
-
-@app.route('/logout/refresh', methods=['POST'])
-def logout_refresh():
-    return {'message': 'Hit the user logout refresh endpoint.'}
-
-
-@app.route('/token/refresh', methods=['POST'])
-def token_refresh():
-    return {'message': 'Hit the token refresh endpoint.'}
-
-
-@app.route('/password/reset', methods=['POST'])
-def reset_password():
-    return {'message': 'Hit the user password reset endpoint.'}
