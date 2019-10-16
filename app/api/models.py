c1d1ce3b1bea4fdea483d18443171c19a695dd8a
Xiyue Li <lxy981126@outlook.com>
2019-10-16 17:30:05 -0400

    Added item model

--- app/api/models.py
+++ app/api/models.py
@@ -20,3 +20,30 @@ class UserAuthModel(db.Model):
     @classmethod
     def find_by_useremail(cls, email):
         return cls.query.filter_by(useremail=email).first()
+
+
+class ItemModel(db.Model):
+    __tablename__ = 'itemInfo'
+
+    itemId = db.Column(db.Integer, primary_key=True)
+    itemName = db.Column(db.String(120), unique=True, nullable=False)
+    price = db.Column(db.Float, unique=True, nullable=False)
+    category = db.Column(db.String(120), nullable=False)
+    brand = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(512), nullable=True)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_category(cls, category):
+        return cls.query.filter_by(category=category).first()
+
+    @classmethod
+    def find_by_brand(cls, brand):
+        return cls.query.filter_by(brand=brand).first()
+
+    @classmethod
+    def find_by_price(cls, price):
+        return cls.query.filter_by(price <= price).first()
