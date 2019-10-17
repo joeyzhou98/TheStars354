ec744114d429b619c35b94dde682f7aa26b1f1b8
Xiyue Li <lxy981126@outlook.com>
2019-10-17 14:00:42 -0400

    Added buyer related models
    Buyer, shopping cart, review, wishlist, order, address.

--- app/api/models.py
+++ app/api/models.py
@@ -22,6 +22,97 @@ class UserAuthModel(db.Model):
         return cls.query.filter_by(useremail=email).first()
 
 
+class BuyerModel(UserAuthModel):
+    __tablename__ = "buyerInfo"
+
+    paypal = db.Column(db.String(20), nullable=False)
+
+    address = db.relationship("address", back_populates="buyer")
+    order_history = db.relationship("order", back_populates="orderNumber")
+    review_history = db.relationship("review", back_populates="review")
+    wish_list = db.relationship("wishList", back_populates="buyer")
+
+    @classmethod
+    def get_address(cls, uid):
+        return cls.query.filter_by(uid=uid)
+
+    @classmethod
+    def get_order_history(cls, uid):
+        return cls.query.filter_by(uid=uid)
+
+
+class Address(db.Model):
+    __tablename__ = "address"
+
+    uid = db.Column(db.Integer, primary_key=True)
+    address1 = db.Column(db.String)
+    address2 = db.Column(db.String)
+    address3 = db.Column(db.String)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Order(db.Model):
+    __tablename__ = "order"
+
+    orderNumber = db.Column(db.Integer, primary_key=True)
+    buyer_id = db.Column(db.Integer, db.ForeignKey("buyerInfo.uid"), nullable=False)
+    seller_id = db.Column(db.Integer, db.ForeignKey("sellerInfo.uid"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("itemInfo.itemId"), nullable=False)
+
+    buyer = db.relationship("buyerInfo", back_populates="uid")
+    seller = db.relationship("sellerInfo", back_populates="uid")
+    item = db.relationship("itemInfo", back_populates="itemId")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class ShoppingCart(db.Model):
+    __tablename__ = "order"
+
+    uid = db.Column(db.Integer, db.ForeignKey("buyerInfo.uid"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("itemInfo.itemId"), nullable=False)
+
+    buyer = db.relationship("buyerInfo", back_populates="address")
+    item = db.relationship("itemInfo")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Review(db.Model):
+    __tablename__ = "review"
+
+    uid = db.Column(db.Integer, db.ForeignKey("buyerInfo.uid"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("itemInfo.itemId"), nullable=False)
+
+    buyer = db.relationship("buyerInfo", back_populates="address")
+    item = db.relationship("itemInfo")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class WishList(db.Model):
+    __tablename__ = "wishList"
+
+    uid = db.Column(db.Integer, db.ForeignKey("buyerInfo.uid"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("itemInfo.itemId"), nullable=False)
+
+    buyer = db.relationship("buyerInfo", back_populates="address")
+    item = db.relationship("itemInfo")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
 class ItemModel(db.Model):
     __tablename__ = 'itemInfo'
 
@@ -38,12 +129,12 @@ class ItemModel(db.Model):
 
     @classmethod
     def find_by_category(cls, category):
-        return cls.query.filter_by(category=category).first()
+        return cls.query.filter_by(category=category)
 
     @classmethod
     def find_by_brand(cls, brand):
-        return cls.query.filter_by(brand=brand).first()
+        return cls.query.filter_by(brand=brand)
 
     @classmethod
     def find_by_price(cls, price):
-        return cls.query.filter_by(price <= price).first()
+        return cls.query.filter(cls.price <= price)
