5656a6146487e2e88c84fef53e944474fec48557
joeyzhou98 <joeyzhou7002@gmail.com>
2019-10-27 05:03:15 -0400

    change import and setup order

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, current_app, send_file
 from flask_sqlalchemy import SQLAlchemy
-from .config import Config
+
 from .client import client_bp
 
 app = Flask(__name__, static_folder='../dist/static')
-#need to set up database first before import api_bp
+# Need to set up database first before import api_bp
+
+from .config import Config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://comp354:' + Config.DATABASE_PASSWORD + '@comp354.cha9oynmedpn.us-east-2.rds.amazonaws.com:3306/comp354'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -14,7 +16,6 @@ from .api import api_bp
 
 app.register_blueprint(api_bp)
 # app.register_blueprint(client_bp)
-
 
 app.logger.info('>>> {}'.format(Config.FLASK_ENV))
 
