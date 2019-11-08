73ceb9108262c73567c52d5dc7adc382e7767b98
joeyzhou98 <joeyzhou7002@gmail.com>
2019-11-08 02:53:26 -0500

    implement showing reviews in frontend plus stars according to rating in item details and item grid

---
+++ src/components/Review.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-card>
+    <star-rating :starStyle="starStyle" :rating="review.rating" :isIndicatorActive="false"></star-rating>
+    <b-card-text v-if="review.content !== null" style="display: flex; padding-top: 15px;">{{review.content}}</b-card-text>
+  </b-card>
+</template>
+
+<script>
+import StarRating from 'vue-dynamic-star-rating'
+
+export default {
+  name: 'Review',
+  components: {
+    StarRating
+  },
+  props: ['review'],
+  data () {
+    return {
+      starStyle: {
+        starWidth: 20,
+        starHeight: 20
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
