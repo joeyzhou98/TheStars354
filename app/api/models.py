15a1789bfaee92a4a7d7c43733eb852dd6ecc407
Xiyue Li <lxy981126@outlook.com>
2019-11-22 17:10:36 -0500

    fixed the shopping list quantity bug

--- app/api/models.py
+++ app/api/models.py
@@ -105,10 +105,9 @@ class BuyerModel(db.Model):
         list_item = db.session.query(shoppingListItem).filter_by(buyer_id=self.uid, item_id=item.item_id)
         if not list_item.count() == 0:
             new_quantity = list_item.first().quantity + 1
-            db.engine.execute(db.update(shoppingListItem)
-                              .where(shoppingListItem.c.buyer_id == self.uid and
-                                     shoppingListItem.c.item_id == item.item_id)
-                              .values(quantity=new_quantity))
+            if list_item.count() != 1:
+                return
+            list_item.update({"quantity": new_quantity}, synchronize_session=False)
         else:
             self.shopping_list.append(item)
         db.session.commit()
