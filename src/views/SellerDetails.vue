35b1f31b9f8bee92802bd749dd31dd75fa326fd3
joeyzhou98 <joeyzhou7002@gmail.com>
2019-12-01 02:05:20 -0500

    added basic public seller details frontend route

---
+++ src/views/SellerDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>SellerDetails page</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'SellerDetails',
+  components: {},
+  data () {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
